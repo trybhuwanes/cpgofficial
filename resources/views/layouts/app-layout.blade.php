35637d483308354eaeb478301472ec32b4d5d7b3
MellityaSilviandroHening <mellityatya@gmail.com>
2024-03-17 19:28:02 +0700

    Try Applying font style

--- resources/views/layouts/app-layout.blade.php
+++ resources/views/layouts/app-layout.blade.php
@@ -2,6 +2,8 @@
 <html>
 <head>
     <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <header>
