537b9128255bd1faebfd1b74fce1f23ae84b93bd
trybhuwanes <trybhuwanes@gmail.com>
2024-03-18 00:58:22 +0700

    fix hero img, footer, navbar, font

--- resources/views/blogPage.blade.php
+++ resources/views/blogPage.blade.php
@@ -5,16 +5,14 @@
 @section('content')
 
     {{-- SECTION 1 --}}
-    <div class="bg-orange-500 max-w-full">
-        <div class="bg-orange-500 max-w-full">
-            <div class="h-[330px] bg-cover bg-center flex justify-center items-center"
-                style="background-image: url('/images/tem-bg.jpg')">
-                <div class="text-4xl font-semibold text-black max-w-[580px] text-center">
-                    Blog
-                </div>
+    <div class="items-center justify-center bg-white">
+        <div class="w-full h-72 shadow-2xl overflow-hidden relative">
+            <img class="absolute inset-0 h-full w-full object-cover" src="images\hero image.png">
+            <div class="absolute inset-0 bg-white bg-opacity-50"></div>
+            <div class="flex h-full items-center justify-center relative">
+                <h1 class="text-4xl text-black font-semibold">BLOG</h1>
             </div>
         </div>
-
     </div>
 
     {{-- SECTION 2 --}}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
     theme: {
       extend: {
         fontFamily: {
-            inter: ['Inter', 'sans-serif'],
+            body: ['Inter Tight'],
           },
       },
     },
