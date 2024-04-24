eb8c4a9a534232ea7fdc337ac43a6548f8565507
MellityaSilviandroHening <mellityatya@gmail.com>
2024-04-24 16:09:54 +0700

    update training

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
 
         $this->call(BlogSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(TrainingSeeder::class);
 
         // \App\Models\User::factory(10)->create();
 
