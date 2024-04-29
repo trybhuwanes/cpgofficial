d45a0b88d5c6ead6926bd4efae8be52dc2720cff
MellityaSilviandroHening <mellityatya@gmail.com>
2024-04-29 14:30:48 +0700

    update after merge

---
+++ resources/views/admin/training/trainingUpdate.blade.php
@@ -0,0 +1,77 @@
+@extends('layouts.admin-app')
+
+@section('title', 'Admin | Create Training Program')
+
+@section('content')
+
+@include('components.component')
+
+
+    {{-- FORM --}}
+    <div class="mt-4 mx-8 space-y-6">
+        <form action="{{ route( 'admin.training.edit', $training->id)}}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
+            <div class="mb-5">
+                <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+                <input type="text" id="title_training" name="title_training"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="Write down the title" value="{{ $training->title_training}}" required />
+            </div>
+
+            <div class="mb-5">
+                <label for="desc" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+                <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+                    <div class="px-4 py-2 bg-white rounded-lg dark:bg-gray-800">
+                        <label for="editor" class="sr-only">Publish post</label>
+                        <textarea id="editor" rows="8" name="desc_training"
+                            class="block w-full px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+                            placeholder="Write an article..."  required>{{ $training->desc_training}}</textarea>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-5">
+                <label for="desc" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Short Description</label>
+                <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
+                    <div class="px-4 py-2 bg-white rounded-lg dark:bg-gray-800">
+                        <label for="editor" class="sr-only">Publish post</label>
+                        <textarea id="editor" rows="8" name="shortdesc_training"
+                            class="block w-full h-10 px-0 text-sm text-gray-800 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+                            placeholder="Write an article..."  required>{{ $training->shortdesc_training}}</textarea>
+                    </div>
+                </div>
+            </div>
+
+            <div class="mb-5">
+                <label for="date_training" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+                <input type="date" id="date_training" name="date_training"
+                class=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                value="{{ $training->date_training}}" required />
+            </div>
+
+            <div class="mb-5">
+                <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Location</label>
+                <input type="text" id="title" name="location_training"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="Write down the building name and its address" value="{{ $training->location_training}}" required />
+            </div>
+{{--
+            <div class="mb-5">
+                <label for="photo" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Photo</label>
+                <input name="pict_training"
+                    class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400"
+                    aria-describedby="photo" id="photo" type="file">
+            </div> --}}
+
+
+
+
+
+            <button type="submit" class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
+                Post
+            </button>
+        </form>
+    </div>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\OrganizingController;
 use App\Http\Controllers\InternshipController;
 use App\Http\Controllers\ContactUsController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\TrainingController;
 use App\Http\Controllers\Auth\AuthenticatedSessionController;
 /*
 |--------------------------------------------------------------------------
@@ -19,8 +20,6 @@ use App\Http\Controllers\Auth\AuthenticatedSessionController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-$this->urlAdmin = "admin";
 
 Route::get('/', function () {
     return view('welcome');
@@ -39,9 +38,7 @@ Route::get('/schedule', function () {
 });
 
 Route::get('/blog', [BlogController::class, 'index']);
-Route::get('/blog-read', [BlogController::class, 'read'])->name('blog-read');
 Route::get('/cerita-umkm', [CeritaUmkmController::class, 'index']);
-Route::get('/cerita-umkm-read', [CeritaUmkmController::class, 'read'])->name('cerita-umkm-read');
 Route::get('/assesment-center', [AssesmentController::class, 'index']);
 Route::get('/organizing', [OrganizingController::class, 'index']);
 Route::get('/internship', [InternshipController::class, 'index']);
@@ -69,6 +66,16 @@ Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])->midd
                 Route::get('/edit/{id}', [BlogController::class, 'editBlogPage'])->name('admin.blog.edit.page');
                 Route::put('/edit/{id}', [BlogController::class, 'editBlog'])->name('edit');
                 Route::delete('/delete/{id}', [BlogController::class, 'deleteBlog'])->name('admin.blog.delete');
+
+            });
+
+            Route::prefix('training')->group(function(){
+                Route::get('/', [TrainingController::class, 'adminTraining'])->name('admin.training');
+                Route::get('/create', [TrainingController::class, 'createTrainingPage'])->name('admin.training.create.page');
+                Route::post('/create', [TrainingController::class, 'createTraining'])->name('admin.training.create');
+                Route::get('/edit/{id}', [TrainingController::class, 'editTrainingPage'])->name('admin.training.edit.page');
+                Route::put('/edit/{id}', [TrainingController::class, 'editTraining'])->name('admin.training.edit');
+                Route::delete('/delete/{id}', [TrainingController::class, 'deleteTraining'])->name('admin.training.delete');
 
             });
 
