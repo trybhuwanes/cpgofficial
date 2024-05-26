9d9e649974fa69fa7c5fc9ad868918ef3f9fef08
trybhuwanes <trybhuwanes@gmail.com>
2024-05-26 09:34:44 +0700

    change photo intern

--- resources/views/internshipPage.blade.php
+++ resources/views/internshipPage.blade.php
@@ -52,7 +52,7 @@
     <div data-aos="fade-up" data-aos-duration="1500" class="grid xl:grid-cols-3 sm:grid-cols-2 gap-4 mx-36">
         {{-- CARD FOTO 1 --}}
         <div class="h-auto bg-white border border-gray-200 rounded-lg shadow-2xl dark:bg-white dark:border-gray-100">
-            <img class="block h-36 object-cover w-full rounded-t-lg" src="images\internbg.png" alt="" />
+            <img class="block h-36 object-cover w-full rounded-t-lg" src="{{ asset('images\internship\internbg.png') }}" alt="" />
             <div class="px-5 py-3">
                 <h6 class="text-lg font-bold tracking-tight text-black dark:text-black">MSIB Batch 6
                 </h6>
@@ -72,7 +72,7 @@
     <div data-aos="fade-up" data-aos-duration="1500" class="grid xl:grid-cols-3 sm:grid-cols-2 gap-4 mx-36 mb-16">
         {{-- CARD FOTO 2 --}}
         <div class="h-auto bg-white border border-gray-200 rounded-lg shadow-2xl dark:bg-white dark:border-gray-100">
-            <img class="block h-36 object-cover w-full rounded-t-lg" src="images\magang-smk-1.jpg" alt="Magang SMK Blabla" />
+            <img class="block h-36 object-cover w-full rounded-t-lg" src="{{ asset('images\internship\magang-smk-1.jpg') }}" alt="Magang SMK Blabla" />
             <div class="px-5 py-3">
                 <h6 class="text-lg font-bold tracking-tight text-black dark:text-black">Magang SMK OTKP/MPLB
                 </h6>
